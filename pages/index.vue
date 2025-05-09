a358c2c22a06a3aefded871be5a98fcc4cd35ecb
Farhan Septiansyah Hidayat <farhanshidayat@gmail.com>
2025-05-09 23:40:12 +0700

    Updating AboutIndex

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <script lang="ts" setup>
 const {t, localeRoute} = useI18n()
 
-const taglines = computed(() => t("profile.taglines"))
-const {y} = useWindowScroll()
-
+const taglines = computed(() => t('profile.taglines'))
 
 </script>
 
 
-<template>
+<template id="container">
   <section id="heroIndex">
     <hero-section class="hero-section">
-      <div class="grid grid-rows-2 min-h-screen">
+      <div class="grid grid-rows-2 min-h-screen" ref="test">
         <div class="tagline-box">
           <h1 v-gsap.delay-100.fromTo="[{opacity:0,x:-20},{opacity:1,x:0}]">Creative By Farhan</h1>
           <div class="head-tagline">
@@ -57,36 +55,38 @@ const {y} = useWindowScroll()
             </NuxtMarquee>
           </div>
         </div>
-
       </div>
     </hero-section>
   </section>
-  <section id="aboutIndex" class="min-h-screen sm:pt-16">
-    <div class="grid flex-col p-6 min-h-screen">
-      <div class="flex justify-center items-center flex-col"
-           v-gsap.whenVisible.fromTo="[{autoAlpha: 0, y:100},{autoAlpha: 1,y:0}]">
-        <h3>{{ $t('profile.title') }}</h3>
-        <div class="text-center pt-6 flex justify-center items-center flex-col max-w-md">
-          <h2 class="sm:text-5xl text-4xl glitch">
-            <span>{{ $t('profile.big_text') }}</span>
-            {{ $t('profile.big_text') }}
-            <span>{{ $t('profile.big_text') }}</span>
-          </h2>
-          <p class=" sm:mt-2 my-6">
-            {{ $t('profile.description') }}
-          </p>
-        </div>
+  <section id="aboutIndex">
+    <div class="main-grid">
+      <div class="title-box">
+        <h2>{{ $t('profile.title') }}</h2>
       </div>
-      <NuxtLink :to="localeRoute('/about')" class="cta-about"
-                v-gsap.whenVisible.fromTo="[{autoAlpha: 0, y:100},{autoAlpha: 1,y:0}]">
-        <div class="flex flex-col justify-center items-center">
-          <div v-for="{title, description} in taglines" class=" tagline" :key="title">
+      <div class="border grid justify-center items-center col-span-2">
+        <h1>
+          {{ $t('profile.big_text') }}
+        </h1>
+      </div>
+      <div class=" grid col-span-2 border justify-center items-center sm:px-12 px-2">
+        <p>
+          {{ $t('profile.description') }}
+        </p>
+      </div>
+      <div class="flex flex-col col-span-2 row-span-2 max-w-full">
+        <div class="box-tagline group" v-for="{title,description} in taglines">
+          <h3 class="opacity-100 group-hover:opacity-0">{{ title }}</h3>
+          <div
+              class="bg-yellow-100 absolute w-full h-full opacity-0 group-hover:opacity-100 flex flex-col items-center gap-2 p-3 transition duration-200">
             <h3>{{ title }}</h3>
-            <p>{{ description }}</p>
+            <p class="text-center max-w-xl">{{ description }}</p>
           </div>
         </div>
-        <div class="flex justify-center items-center flex-col my-3 sm:my-0 border sm:border-none py-6 sm:py-0">
-          <h2 class=" sm:text-3xl text-xl font-semibold!">{{ $t('profile.about_cta') }}</h2>
+      </div>
+      <NuxtLink :to="localeRoute('/about')"
+                class=" cta-about">
+        <div class="cta-textArea">
+          <h3 class="">{{ $t('profile.about_cta') }}</h3>
           <svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M62.3431 13.6569L68 8.00001" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
             <path d="M2 8H68" stroke="currentColor" stroke-width="4" stroke-linecap="round"/>
@@ -94,15 +94,21 @@ const {y} = useWindowScroll()
           </svg>
         </div>
       </NuxtLink>
-      <div class="flex flex-col justify-center items-center pb-3 h-full">
-        <h3>{{ $t('profile.experience') }}</h3>
-        <div class="flex sm:gap-32 gap-16 justify-center items-center sm:mt-3 mt-3">
-          <img v-gsap.whileVisible.to="{ scale : 0.5, duration : 1, repeat : 1,yoyo: true }" width="120"
-               src="/public/company-logo/Sena.svg" alt="sena">
-          <img v-gsap.whileVisible.to="{ scale : 0.5, duration : 1, repeat : 1,yoyo: true }" width="120"
-               src="/public/company-logo/Samsung.svg" alt="samsung">
-          <img v-gsap.whileVisible.to="{ scale : 0.5, duration : 1, repeat : 1,yoyo: true }" width="120"
-               src="/public/company-logo/H&M.svg" alt="h&m">
+      <div class="title-box">
+        <h2>{{ $t('profile.experience') }}</h2>
+      </div>
+      <div class="grid grid-cols-3 col-span-4 h-full">
+        <div class="grid col-span-1 justify-center items-center border relative group">
+          <img src="/public/company-logo/Sena.svg" alt="sena">
+          <div class="absolute opacity-0 grid bg-yellow-200 h-full w-full group-hover:opacity-100">
+            <h3>Test</h3>
+          </div>
+        </div>
+        <div class="grid col-span-1 p-3 justify-center items-center border">
+          <img src="/public/company-logo/Samsung.svg" alt="samsung">
+        </div>
+        <div class="grid col-span-1 justify-center items-center border">
+          <img src="/public/company-logo/H&M.svg" alt="h&m">
         </div>
       </div>
     </div>
@@ -115,7 +121,7 @@ const {y} = useWindowScroll()
   @apply sm:px-10 px-6 flex flex-col justify-center;
 
   h1 {
-    @apply sm:text-6xl text-3xl font-bold;
+    @apply sm:text-8xl text-3xl font-bold;
   }
 
   .head-tagline {
@@ -148,114 +154,71 @@ const {y} = useWindowScroll()
     @apply flex flex-col items-center justify-center row-span-3 cursor-pointer;
   }
 
-}
+  .running-text {
+    @apply flex items-end justify-end pb-6 overflow-hidden text-white mt-12 w-full;
 
-.running-text {
-  @apply flex items-end justify-end pb-6 overflow-hidden text-white mt-12 w-full;
-
-  p {
-    @apply text-xs sm:text-sm;
+    p {
+      @apply text-xs sm:text-sm;
+    }
   }
+
 }
+
 
 #aboutIndex {
-  h2 {
-    @apply sm:text-3xl text-xl font-semibold
-  }
 
-  h3 {
-    @apply sm:text-xl text-sm font-semibold
-  }
+  .main-grid {
+    @apply grid grid-cols-4 min-h-screen max-w-full ;
 
-  p {
-    @apply text-sm sm:text-base;
-  }
+    .title-box {
+      @apply border grid col-span-4 justify-center items-center;
+    }
 
-  .tagline {
-    @apply max-w-lg sm:mb-12 mb-6 flex flex-col gap-2;
+    h1 {
+      @apply sm:text-6xl text-xl sm:font-semibold font-medium;
+    }
+
+    h2 {
+      @apply sm:text-2xl text-xl sm:font-semibold font-medium;
+    }
+
+    h3 {
+      @apply sm:text-xl text-sm sm:font-semibold font-medium;
+    }
 
     p {
-      @apply text-base sm:text-base text-justify;
+      @apply sm:text-2xl text-xs font-light;
     }
-  }
 
-  .cta-about {
-    @apply grid sm:grid-cols-2 sm:gap-2 sm:my-0 lg:px-8 py-3;
-    color: #161616;
-
-    p {
-      @apply text-black
+    img {
+      @apply w-16 sm:w-32;
     }
-  }
 
-  .cta-about:hover {
-    background: #F1F1F1;
-    color: #317ABE;
-    transition: ease 0.7s;
+    .box-tagline {
+      @apply relative sm:p-3 p-1 border flex justify-center items-center col-span-2 cursor-pointer h-full;
+
+      p {
+        @apply text-sm;
+      }
+    }
+
+    .cta-about{
+      @apply  border grid row-span-2 col-span-2 justify-center items-center w-full;
+
+      .cta-textArea {
+        @apply flex flex-col justify-center items-center gap-3;
+      }
+    }
+
+    .cta-about:hover {
+     @apply transition duration-200 ease-in-out;
+      background: #317ABE;
+      color: #DDDDDD;
+    }
+
   }
 
 }
-
-.glitch {
-  @apply relative;
-  text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
-  0.025em 0.04em 0 #fffc00;
-  animation: glitch 725ms infinite;
-  h2 {
-    @apply sm:text-6xl text-xl font-semibold italic
-  }
-}
-
-.glitch span {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-.glitch span:first-child {
-  animation: glitch 500ms infinite;
-  clip-path: polygon(0 0, 100% 0, 100% 35%, 0 35%);
-  transform: translate(-0.04em, -0.03em);
-  opacity: 0.75;
-}
-
-.glitch span:last-child {
-  animation: glitch 375ms infinite;
-  clip-path: polygon(0 65%, 100% 65%, 100% 100%, 0 100%);
-  transform: translate(0.04em, 0.03em);
-  opacity: 0.75;
-}
-
-@keyframes glitch {
-  0% {
-    text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
-    0.025em 0.04em 0 #fffc00;
-  }
-  15% {
-    text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
-    0.025em 0.04em 0 #fffc00;
-  }
-  16% {
-    text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff,
-    -0.05em -0.05em 0 #fffc00;
-  }
-  49% {
-    text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff,
-    -0.05em -0.05em 0 #fffc00;
-  }
-  50% {
-    text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
-    0 -0.04em 0 #fffc00;
-  }
-  99% {
-    text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
-    0 -0.04em 0 #fffc00;
-  }
-  100% {
-    text-shadow: -0.05em 0 0 #00fffc, -0.025em -0.04em 0 #fc00ff,
-    -0.04em -0.025em 0 #fffc00;
-  }
-}
-
 
 
 </style>
